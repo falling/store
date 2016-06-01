50a06c763043d75944919e65c3e8b23b7b1feea6
zyz_falling <zyz635857938@gmail.com>
2016-06-01 10:19:34 +0800

    通用DAO

--- src/dao/dao.java
+++ src/dao/dao.java
@@ -8,10 +8,28 @@ import java.util.List;
  * Created by falling on 16/5/28.
  */
 public interface DAO {
+    /**
+     * 保存一条记录
+     * @param bean 要增加的记录的bean
+     */
     void save(Bean bean);
-    void delete();
-    void update();
-    Object get();
-    List getForList();
 
+    /**
+     * 删除一条记录
+     * @param bean bean里需要有id的值
+     */
+    void delete(Bean bean);
+
+    /**
+     * 更改一条记录
+     * @param bean bean是新的一个对象
+     */
+    void update(Bean bean);
+
+    /**
+     * 根据id获取一个对象
+     * @param bean bean里需要有id的值
+     * @return
+     */
+    Object get(Bean bean);
 }
